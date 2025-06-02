9fb371182dfbeb1ff908a6834dbd0c518762efe9
unknown <General@HPLASB.NS.com>
2025-06-01 20:44:47 -0600

    Inclui paginas de redireccion gracias

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,22 +1,17 @@
-import Stripe from "stripe";
-
-const stripe = new Stripe(
-  "ssk_live_51RV5boLn2X7paUwsGgAlz48a1e8sdjKYxhNdYDajrpAo599zLtw5XdLaZorStNmexWXC0uA6Zix3pzKaWW2UmzxO00DOGvFUCf"
-);
-
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).send("Método no permitido");
   }
 
-  const { priceId, planKey, periodo, customerEmail } = req.body;
-
   try {
+    console.log("BODY RECIBIDO:", req.body); // 👈 Para depurar
+    const { priceId, planKey, periodo, customerEmail } = req.body;
+
     const session = await stripe.checkout.sessions.create({
       line_items: [{ price: priceId, quantity: 1 }],
       mode: "subscription",
       success_url: `${req.headers.origin}/gracias-${planKey}`,
-      cancel_url: `${req.headers.origin}#planes`,
+      cancel_url: `${req.headers.origin}/#planes`,
       customer_email: customerEmail,
       metadata: {
         plan: planKey,
@@ -25,7 +20,8 @@ export default async function handler(req, res) {
     });
 
     res.status(200).json({ id: session.id });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+  } catch (error) {
+    console.error("Error Stripe:", error); // 👈 Muy útil
+    res.status(500).send("A server error occurred");
   }
 }
